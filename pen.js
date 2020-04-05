ef3a2b5b107b7b6d9a08570b85e4c227ba5d87f2
saths008 <saths008@users.noreply.github.com>
2020-04-05 15:06:59 +0100

    First Commit
    First website including jumbotron and an image slider

---
+++ pen.js
@@ -0,0 +1,31 @@
+var listofanimes=[
+    "Naruto",
+"Fairy tail",
+"Seikoku no Doraguna",
+"Hosizora e kakaru hashi",
+"seirei tsukei no bureidodansu",
+"zero no tsukaima",
+"sakura sou no pet kanojo",
+"yamada-kun no 7 min no majo",
+"isekai wa sumatopon",
+"re zero",
+"seiren",
+"Maid-sama",
+"koi to sakyo to chocolate",
+"masamune-kun no revenge",
+"campiogne",
+"hundred",
+"Rakudai kishi no cavalry",
+"roku de nashi majutsu koushi to akashic records",
+"sword art online",
+"strike the blood",
+"nijiro days",
+"trinity seven",
+"seiken tsukai no world break",
+"gakusen toshi asterix",
+"gamers!",
+"aoharu ride",
+];
+
+var randomItem = listofanimes[Math.floor(Math.random()*listofanimes.length)];
+ console.log("pen.js")
